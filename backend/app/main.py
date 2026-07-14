920f9f734780143b1d95b293c001cef6c433d5a2
malsalem3 <malsalem3.gatech.edu>
2026-07-13 22:40:29 -0400

    docker setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Clinical Triage AI API",
     version="1.0.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
