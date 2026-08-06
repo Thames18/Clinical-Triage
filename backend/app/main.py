bed25c3006a09bafc6a0e232079ee5ac74013129
malsalem3 <malsalem3.gatech.edu>
2026-08-06 15:27:29 -0400

    Remove exposed private key, add pem to gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,43 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from app.schemas.triage import PatientInput
 from app.services.llm import analyze_patient
 from app.api.routes import report, auth
 from app.core.dependency import get_current_user
 
-app = FastAPI(
-    title="Clinical Triage AI API",
-    version="1.0.0"
-)
-
-app.include_router(report.router)
+app = FastAPI(title="Clinical Triage AI API", 
+              version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000"
-    ],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-def root():
-    return {
-        "message": "Clinical Triage API"
-    }
-
-
-@app.get("/health")
-def health():
-    return {
-        "status": "healthy"
-    }
-
 app.include_router(auth.router)
 app.include_router(report.router, dependencies=[Depends(get_current_user)])
 
+@app.get("/")
+def root():
+    return {"message": "Clinical Triage API"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
 
 @app.post("/triage")
 def triage(patient: PatientInput, user: str = Depends(get_current_user)):
