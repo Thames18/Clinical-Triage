9d982bbcb0b2f851b4d328c2518fd38173b70187
thames18 <mustafaalaa@live.com>
2026-08-07 12:27:57 -0400

    test sample added

---
+++ backend/tests/test_triage.py
@@ -0,0 +1,16 @@
+from app.services.triage_service import analyze_patient
+from app.schemas.triage import PatientInput
+def test_low oxygen():
+    patient=PatientInput(
+        age=60,
+        symptoms=[
+            "shortness of breath"
+        ],
+        oxygen_saturation=85
+    )
+    result=analyze_patient(patient)
+    assert (
+        result.triage_level
+        ==
+        "EMERGENCY"
+    )
